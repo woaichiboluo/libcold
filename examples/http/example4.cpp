85916f06980355e8897fccd49bca6529736d484a
woaichiboluo <147318851+woaichiboluo@users.noreply.github.com>
2024-08-18 02:03:37 +0800

    add websocket echo example

--- examples/http/example4.cpp
+++ examples/http/example4.cpp
@@ -1,3 +1,4 @@
+#include "WebSocketEchoPage.h"
 #include "cold/net/http/HttpServer.h"
 #include "cold/net/http/WebSocket.h"
 
@@ -6,17 +7,33 @@ using namespace Cold;
 class EchoWebSocketServer : public Net::Http::WebSocketServer {
  public:
   EchoWebSocketServer() = default;
-  ~EchoWebSocketServer() override = default;
+  ~EchoWebSocketServer() override {
+    Base::INFO("EchoWebSocketServer destructed");
+  }
 
-  void OnNewWebSocket(Net::Http::WebSocketPtr s) override {
+  void OnConnect(Net::Http::WebSocketPtr s) override {
     s->SetOnRecv([](Net::Http::WebSocket::WebSocketPtr ws, const char* data,
-                    size_t len) { ws->Send(data, len); });
+                    size_t len) {
+      Base::INFO("On Echo recv data length: {}", len);
+      ws->Send(data, len);
+    });
+  }
+
+  void OnClose(Net::Http::WebSocket::WebSocketPtr ws) override {
+    Base::INFO("EchoWebSocket closed");
   }
 };
 
 int main() {
-  auto wsServer = std::make_unique<Net::Http::WebSocketServer>();
+  auto wsServer = std::make_unique<EchoWebSocketServer>();
   Net::Http::HttpServer httpServer(Net::IpAddress(8080));
+  httpServer.AddServlet("/index.html", [](Net::Http::HttpRequest& req,
+                                          Net::Http::HttpResponse& resp) {
+    resp.SetStatus(Net::Http::HttpStatus::OK);
+    auto body = std::make_unique<Net::Http::TextBody>();
+    body->SetContent(kEchoPage);
+    resp.SetBody(std::move(body));
+  });
   httpServer.SetWebSocketServer(std::move(wsServer));
   httpServer.Start();
 }
