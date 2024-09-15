a3321f7a0ccf642c83b4292c9e9e0d9bda1e2efc
woaichiboluo <147318851+woaichiboluo@users.noreply.github.com>
2024-09-16 01:16:45 +0800

    update

--- third_party/doctest.h
+++ third_party/doctest.h
@@ -39,9 +39,6 @@
 // =================================================================================================
 // =================================================================================================
 
-#ifndef THRID_PARTY_DOCTEST
-#define THRID_PARTY_DOCTEST
-
 #ifndef DOCTEST_LIBRARY_INCLUDED
 #define DOCTEST_LIBRARY_INCLUDED
 
@@ -7107,6 +7104,3 @@ DOCTEST_SUPPRESS_COMMON_WARNINGS_POP
 #undef NOMINMAX
 #undef DOCTEST_UNDEF_NOMINMAX
 #endif // DOCTEST_UNDEF_NOMINMAX
-
-
-#endif /* THRID_PARTY_DOCTEST */
