b1b45e06c1a0b431a4798b482b7e0ff8e7f80cd1
woaichiboluo <147318851+woaichiboluo@users.noreply.github.com>
2024-10-02 01:21:51 +0800

    implement scheduler to iocontext

--- cold/io/IoContext.h
+++ cold/io/IoContext.h
@@ -2,9 +2,10 @@
 #define COLD_IO_IOCONTEXT
 
 #include <atomic>
+#include <list>
 #include <memory>
 
-#include "../coroutines/Scheduler.h"
+#include "../coroutines/ThisCoro.h"
 #include "../detail/IoWatcher.h"
 #include "../detail/SigpipeIgnorer.h"
 #include "../detail/TimerQueue.h"
@@ -14,13 +15,16 @@ namespace Cold {
 class IoContext {
  public:
   IoContext()
-      : scheduler_(std::make_unique<Scheduler>()),
-        ioWatcher_(std::make_unique<Detail::IoWatcher>(this)),
+      : ioWatcher_(std::make_unique<Detail::IoWatcher>(this)),
         timerQueue_(std::make_unique<Detail::TimerQueue>()) {
     Detail::IgnoreSigPipe();
   }
 
-  ~IoContext() = default;
+  ~IoContext() {
+    for (const auto& handle : unfinishedCoros_) {
+      handle.destroy();
+    }
+  }
 
   IoContext(const IoContext&) = delete;
   IoContext& operator=(const IoContext&) = delete;
@@ -31,17 +35,14 @@ class IoContext {
     while (running_) {
       ++iterations_;
       TRACE("IoContext Start of this iterations:{}", iterations_);
-      std::vector<Task<>> pendingTasks;
       // solve timeout event
+      std::vector<Task<>> pendingTasks;
+      std::vector<std::coroutine_handle<>> pendingResume;
       {
         std::lock_guard<std::mutex> lock(mutexForTimerQueue_);
         timerQueue_->Tick(pendingTasks);
       }
-      for (auto& task : pendingTasks) {
-        task.GetHandle().promise().SetIoContext(this);
-        scheduler_->CoSpawn(task.Release());
-      }
-      scheduler_->DoSchedule();
+      DoSchedule(pendingTasks, pendingResume);
       // solve io event
       static constexpr int kDefaultWaitMs = 300;
       auto nextTick = timerQueue_->GetNextTick();
@@ -51,27 +52,20 @@ class IoContext {
                       now.TimeSinceEpochMilliSeconds();
       waitMs = static_cast<int>(waitTime);
       waitMs = std::min(waitMs, kDefaultWaitMs);
-      ioWatcher_->WatchIo(scheduler_->GetPendingCoros(), waitMs);
-      scheduler_->DoSchedule();
+      ioWatcher_->WatchIo(pendingResume, waitMs);
+      DoSchedule(pendingTasks, pendingResume);
       // solve pending event
-      pendingTasks.clear();
       {
         std::lock_guard<std::mutex> lock(mutexForPendingTasks_);
-        pendingTasks.swap(pendingTasks_);
+        pendingTasks_.swap(pendingTasks);
       }
       {
         std::lock_guard<std::mutex> lock(mutexForPendingResume_);
-        scheduler_->GetPendingCoros().swap(pendingResume_);
+        pendingResume_.swap(pendingResume);
       }
-      for (auto& task : pendingTasks) {
-        task.GetHandle().promise().SetIoContext(this);
-        scheduler_->CoSpawn(task.Release());
-      }
-      scheduler_->DoSchedule();
+      DoSchedule(pendingTasks, pendingResume);
       TRACE("IoContext End of this iteration:{}. unfinished coros size: {}",
-            iterations_, scheduler_->GetPendingCorosSize(),
-            scheduler_->GetAllCoroSize());
-      assert(scheduler_->GetPendingCoros().empty());
+            iterations_, unfinishedCoros_.size());
     }
   }
 
@@ -84,7 +78,7 @@ class IoContext {
 
   void CoSpawn(Task<> task) {
     {
-      std::lock_guard<std::mutex> lock(mutexForPendingTasks_);
+      std::lock_guard lock(mutexForPendingTasks_);
       pendingTasks_.push_back(std::move(task));
     }
     ioWatcher_->WakeUp();
@@ -92,10 +86,16 @@ class IoContext {
 
   void CoResume(std::coroutine_handle<> handle) {
     {
-      std::lock_guard<std::mutex> lock(mutexForPendingResume_);
+      std::lock_guard lock(mutexForPendingResume_);
       pendingResume_.push_back(handle);
     }
     ioWatcher_->WakeUp();
+  }
+
+  [[nodiscard]] Task<> RunInThisContext() {
+    auto& context = co_await ThisCoro::GetCurExecuteIoContext();
+    if (&context == this) co_return;
+    co_await RunInThisAwaitable(this);
   }
 
   std::shared_ptr<Detail::IoEvent> TakeIoEvent(int fd) {
@@ -129,20 +129,82 @@ class IoContext {
   size_t GetIterations() const { return iterations_; }
 
  private:
+  class RunInThisAwaitable {
+   public:
+    RunInThisAwaitable(IoContext* context) : context_(context) {}
+    ~RunInThisAwaitable() = default;
+
+    bool await_ready() const noexcept { return false; }
+
+    void await_suspend(std::coroutine_handle<> handle) noexcept {
+      context_->CoResume(handle);
+    }
+
+    void await_resume() const noexcept {}
+
+   private:
+    IoContext* context_;
+  };
+
+  void SetCurExecuteContext(std::coroutine_handle<> handle) {
+    auto& promise = std::coroutine_handle<Detail::PromiseBase>::from_address(
+                        handle.address())
+                        .promise();
+    promise.SetCurExecuteIoContext(this);
+  }
+
+  static bool IsCoroStoped(std::coroutine_handle<> handle) {
+    auto& promise = std::coroutine_handle<Detail::PromiseBase>::from_address(
+                        handle.address())
+                        .promise();
+    return promise.GetStopToken().stop_requested();
+  }
+
+  static bool Destoryable(std::coroutine_handle<> handle) {
+    if (handle.done() || IsCoroStoped(handle)) {
+      handle.destroy();
+      return true;
+    }
+    return false;
+  }
+
+  void DoSchedule(std::vector<Task<>>& pendingTasks,
+                  std::vector<std::coroutine_handle<>>& pendingResume) {
+    // solve new task
+    for (auto& task : pendingTasks) {
+      task.GetHandle().promise().SetIoContext(this);
+      auto handle = task.Release();
+      unfinishedCoros_.push_back(handle);
+      pendingResume.push_back(handle);
+    }
+    pendingTasks.clear();
+    // resume pending coros
+    for (auto& handle : pendingResume) {
+      if (IsCoroStoped(handle)) continue;
+      assert(!handle.done());
+      SetCurExecuteContext(handle);
+      handle.resume();
+    }
+    pendingResume.clear();
+    // destory finished coros
+    unfinishedCoros_.remove_if(Destoryable);
+  }
+
   size_t iterations_ = 0;
 
   std::atomic<bool> running_ = false;
-  std::unique_ptr<Scheduler> scheduler_;
   std::unique_ptr<Detail::IoWatcher> ioWatcher_;
-
-  std::mutex mutexForPendingResume_;
-  std::vector<std::coroutine_handle<>> pendingResume_;
 
   std::mutex mutexForTimerQueue_;
   std::unique_ptr<Detail::TimerQueue> timerQueue_;
 
   std::mutex mutexForPendingTasks_;
   std::vector<Task<>> pendingTasks_;
+
+  std::mutex mutexForPendingResume_;
+  std::vector<std::coroutine_handle<>> pendingResume_;
+
+  std::list<std::coroutine_handle<>> unfinishedCoros_;
 };
 
 inline void Timer::Add() { context_->AddTimer(this); }
