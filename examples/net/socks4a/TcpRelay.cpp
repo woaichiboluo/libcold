d7d819b3acacfbffca132f0390d670d49407d87b
woaichiboluo <147318851+woaichiboluo@users.noreply.github.com>
2024-10-03 03:31:10 +0800

    add TcpRelay

---
+++ examples/net/socks4a/TcpRelay.cpp
@@ -0,0 +1,57 @@
+#include "Tunnel.h"
+
+using namespace Cold;
+
+class TcpRelay : public TcpServer {
+ public:
+  TcpRelay(const IpAddress& localAddr, const IpAddress& remoteAddr,
+           size_t poolsize = 0)
+      : TcpServer(localAddr, poolsize, "TcpRelay"), remoteAddr_(remoteAddr) {}
+  ~TcpRelay() override = default;
+
+ private:
+  static std::atomic<int> tunnelId_;
+
+  Task<> DoJob(TcpSocket localSocket) {
+    using namespace std::chrono_literals;
+    TcpSocket remoteSocket(localSocket.GetIoContext());
+    auto [timeout, ret] =
+        co_await Timeout(remoteSocket.Connect(remoteAddr_), 3s);
+    if (timeout || !ret) {
+      ERROR("failed to connect to remote: {}", remoteAddr_.GetIpPort());
+      localSocket.Close();
+      co_return;
+    }
+    ++tunnelId_;
+    // data swap delegate to Tunnel
+    auto tunnel = std::make_shared<Tunnel>(std::move(localSocket),
+                                           std::move(remoteSocket), tunnelId_);
+    tunnel->SwapData();
+  }
+
+  Task<> OnNewConnection(TcpSocket socket) override {
+    INFO("new connection from {}", socket.GetRemoteAddress().GetIpPort());
+    co_await DoJob(std::move(socket));
+  }
+
+  IpAddress remoteAddr_;
+};
+
+std::atomic<int> TcpRelay::tunnelId_{0};
+
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    fmt::println("Usage: {} <listenPort> <remoteAddr> <remotePort>", argv[0]);
+    return 1;
+  }
+  uint16_t listenPort = static_cast<uint16_t>(std::stoi(argv[1]));
+  auto addr = IpAddress::Resolve(argv[2], argv[3]);
+  if (!addr) {
+    fmt::println("cannot resolve remote address: {} port: {}", argv[2],
+                 argv[3]);
+    return 1;
+  }
+  TcpRelay relay(IpAddress(listenPort), *addr, 4);
+  INFO("remote addr: {}", addr->GetIpPort());
+  relay.Start();
+}
---
+++ examples/net/socks4a/Tunnel.h
@@ -0,0 +1,71 @@
+#ifndef EXAMPLES_NET_SOCKS4A_TUNNEL
+#define EXAMPLES_NET_SOCKS4A_TUNNEL
+
+#include "cold/Cold.h"
+
+/**
+ * |          localSocket_           remoteSocket_
+ * | client <--------------> relay <--------------->  server
+ */
+
+class Tunnel : public std::enable_shared_from_this<Tunnel> {
+ public:
+  Tunnel(Cold::TcpSocket localSocket, Cold::TcpSocket remoteSocket,
+         int tunnelId)
+      : localSocket_(std::move(localSocket)),
+        remoteSocket_(std::move(remoteSocket)),
+        tunnelId_(tunnelId) {}
+
+  ~Tunnel() = default;
+
+  Tunnel(const Tunnel&) = delete;
+  Tunnel& operator=(const Tunnel&) = delete;
+
+  void SwapData() {
+    auto self = shared_from_this();
+    assert(&localSocket_.GetIoContext() == &remoteSocket_.GetIoContext());
+    auto& context = localSocket_.GetIoContext();
+    context.CoSpawn(ClientToRelay(self));
+    context.CoSpawn(RelayToClient(self));
+  }
+
+ private:
+  Cold::Task<> ClientToRelay(std::shared_ptr<Tunnel> self) {
+    char buf[5 * 1024 * 1024];
+    while (localSocket_.CanReading()) {
+      auto n = co_await localSocket_.Read(buf, sizeof buf);
+      if (n <= 0) break;
+      auto ret = co_await remoteSocket_.WriteN(buf, static_cast<size_t>(n));
+      if (ret != static_cast<ssize_t>(n)) break;
+      Cold::INFO("tunnelId: {} client -> relay {} bytes", tunnelId_, n);
+    }
+    Stop();
+  }
+
+  Cold::Task<> RelayToClient(std::shared_ptr<Tunnel> self) {
+    char buf[5 * 1024 * 1024];
+    while (remoteSocket_.CanReading()) {
+      auto n = co_await remoteSocket_.Read(buf, sizeof buf);
+      if (n <= 0) break;
+      auto ret = co_await localSocket_.WriteN(buf, static_cast<size_t>(n));
+      if (ret != static_cast<ssize_t>(n)) break;
+      Cold::INFO("tunnelId: {} relay -> client {} bytes", tunnelId_, n);
+    }
+    Stop();
+  }
+
+  void Stop() {
+    if (stoped_) return;
+    Cold::INFO("tunnelId: {} stop", tunnelId_);
+    localSocket_.Close();
+    remoteSocket_.Close();
+    stoped_ = true;
+  }
+
+  Cold::TcpSocket localSocket_;
+  Cold::TcpSocket remoteSocket_;
+  int tunnelId_;
+  bool stoped_ = false;
+};
+
+#endif /* EXAMPLES_NET_SOCKS4A_TUNNEL */
